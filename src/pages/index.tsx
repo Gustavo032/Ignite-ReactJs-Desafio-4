a2a6a338f6bdc8587d1e7651b6b10a869c0ad0c1
Gustavo032 <gustavoramosrock@gmail.com>
2022-08-08 00:23:15 -0300

    finalizando a aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from 'next';
-
+import Link from 'next/link'
 import { getPrismicClient } from '../services/prismic';
-
+import { FiCalendar, FiUser } from 'react-icons/fi'
+import {ptBR} from 'date-fns/locale'
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { format } from 'date-fns';
+import { useState } from 'react';
 
 interface Post {
   uid?: string;
@@ -13,24 +16,109 @@ interface Post {
     subtitle: string;
     author: string;
   };
-}
+};
 
 interface PostPagination {
   next_page: string;
   results: Post[];
-}
+};
 
 interface HomeProps {
   postsPagination: PostPagination;
-}
+};
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({postsPagination} : HomeProps) {
+	const formattedPosts = postsPagination.results.map(post=>({
+		...post,
+		first_publication_date: format(new Date(post.first_publication_date), 'dd MMM yyyy', {
+			locale: ptBR
+		})
+	}))
+	const [posts, setPosts] = useState<Post[]>(formattedPosts)
+	const [nextPage, setNextPage] = useState(postsPagination.next_page)
+	
+	async function handleNextPage(): Promise<void>{
+		if (nextPage === null) return;
+		const postsResults = await fetch(nextPage).then(response => response.json())
+		
+		setNextPage(postsResults.next_page);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+		const newPosts = postsResults.results.map((post: Post) => {
+			return {
+                ...post,
+				first_publication_date: format(new Date(post.first_publication_date), 'dd MMM yyyy', {locale: ptBR})
+			}
+		})
 
-//   // TODO
-// };
+		setPosts([...posts, ...newPosts])
+	}
+
+	const [isLoading, setIsLoading] = useState(false);
+	function loading(){
+		if (isLoading === false){
+			setIsLoading(true);
+		}
+	}
+
+	return(
+	<main className={commonStyles.container}>
+		<div className={styles.posts}>
+			{posts.map(post=>{
+				return(
+					<Link key={post.uid} href={`/post/${post.uid}`}>
+						<a className={styles.post} onClick={loading}>
+							<strong>{post.data.title}</strong>
+							<p>{post.data.subtitle}</p>
+							<ul>
+								<li>
+									<FiCalendar/>
+									{post.first_publication_date}
+								</li>
+								
+								<li>
+									<FiUser />
+									{post.data.author}
+								</li>
+							</ul>
+						</a>
+					</Link>
+				);
+			})}
+
+			{nextPage && (
+				<button type='button' onClick={handleNextPage}>
+					Carregar mais posts
+				</button>
+			)}
+
+			{isLoading && (
+				<p>Carregando...</p>
+			)}
+			
+		</div>
+	</main>
+  );
+};
+
+export const getStaticProps : GetStaticProps = async () => {
+
+  const prismic = getPrismicClient({});
+  
+  const postsResponse = await prismic.getByType("posts", { pageSize: 3,
+	orderings: {
+		field: 'last_publication_date',
+		direction: 'desc',
+	},
+  });
+
+  const postsPagination = {
+	next_page: postsResponse.next_page,
+	results: postsResponse.results
+  };
+
+  return{
+	props: {
+		postsPagination,
+	},
+  };
+};
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,9 @@
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import { RichText } from 'prismic-dom';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,20 +31,90 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({post}: PostProps) {
+	const router = useRouter();
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+	if(router.isFallback){
+		return <h1>Carregando...</h1>;
+	}
 
-//   // TODO
-// };
+	const formattedDate = format(new Date(post.first_publication_date), 'dd MMM yyy', {locale: ptBR})
+	const totalWords = post.data.content.reduce((total, contentItem) =>{
+		const headingTime = contentItem.heading.split(/\s+/).length;
+		const contentTime = RichText.asText(contentItem.body).split(/\s+/).length;
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+		return total + headingTime + contentTime;
+	}, 0)
 
-//   // TODO
-// };
+	const readTime = Math.ceil(totalWords / 200)
+
+  return(
+	<>
+	<img src={post.data.banner.url} alt="Banner" className={styles.banner}/>
+	<main className={commonStyles.container}>
+		<div className={styles.post}>
+			<div className={styles.postTop}>
+				<h1>{post.data.title}</h1>
+				<ul>
+					<li>
+						<FiCalendar/>
+						{formattedDate}
+					</li>
+					<li>
+						<FiUser/>
+						{post.data.author}
+					</li>
+					<li>
+						<FiClock/>
+						{readTime} min
+					</li>
+				</ul>
+			</div>
+
+			{post.data.content.map((content) =>{
+				return(
+                    <article key={content.heading}>
+						<h2>{content.heading}</h2>
+						<div
+						className={styles.postContent}
+						dangerouslySetInnerHTML={{
+							__html: RichText.asHtml(content.body)
+						}}/>
+					</article>
+				)
+			})}
+		</div>
+	</main>
+	</>
+  );
+}
+
+export const getStaticPaths:GetStaticPaths = async () => {
+  const prismic = getPrismicClient({});
+  const posts = await prismic.getByType('posts');
+	const paths = posts.results.map(post => { 
+		return{
+			params: {
+				slug: post.uid,
+			},
+		};
+	})
+
+  return {
+	paths,
+	fallback: true
+  }
+};
+
+export const getStaticProps: GetStaticProps = async ({params }) => {
+	const {slug} = params;
+  const prismic = getPrismicClient({});
+  const response = await prismic.getByUID('posts', String(slug));
+
+  return{
+	props: {
+		post: response
+	},
+	revalidate: 1800
+}
+};
